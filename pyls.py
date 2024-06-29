f693598fbd3a04b750dbf8ec9f275f899df5bce2
Pritam Samanta <undermine360@gmail.com>
2024-06-29 14:48:29 +0530

    added all the arguments handlers and updated the tests

--- pyls.py
+++ pyls.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime
 
 # base path of the project directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,14 +13,84 @@ def read_json(json_file: str)-> dict:
         json_data=json.load(f)
     return json_data
 
+# handles path
+def handle_path(dir_: dict, search_key: str)-> dict:
+    if search_key in [".", "./"]:
+        return dir_
+    dir_name=search_key.split("/")
+    if len(dir_name)>1:
+        full_path=""
+        # recursively finds target dir/file if nested path is given
+        for dir_nam in dir_name:
+            dir_=handle_path(dir_,dir_nam)
+            # returns None if valid path not found
+            if dir_ is None:
+                return
+            full_path+=f'/{dir_["name"]}'
+            if full_path.strip("/") == search_key:
+                dir_["name"]=search_key
+                return dir_
+    if dir_["name"]==search_key:
+        # returns dir/filename from current dir given as input path
+        return dir_
+    if "contents" in dir_.keys():
+        # finds sub-dir/file under current dir given as input path
+        for sub_dir in dir_["contents"]:
+            if sub_dir["name"]==search_key:
+                return sub_dir
+
 def read_info(**kwargs: dict)-> str:
     json_data = read_json(JSON_FILE_PATH)
     contents=json_data["contents"]
+    if kwargs["path"]:
+        # handles target path if arg supplied from cli
+        contents=handle_path(json_data, kwargs["path"])
+        print(contents)
+        if contents is None:
+            # returns error message on incorrect path
+            return f"error: cannot access '{kwargs['path']}': No such file or directory"
+        contents=contents["contents"] if "contents" in contents.keys() else [contents]
     if kwargs["A"]==False:
         # by default it removes filenames starts with "."
         # when -A argument given, it doesn't remove the hidden files from output
         contents=[content for content in contents if not content["name"].startswith(".")]
+    if kwargs["filter"]:
+        if kwargs["filter"]=="dir":
+            # returns only directories
+            contents=[content for content in contents if "contents" in content.keys()]
+        elif kwargs["filter"]=="file":
+            # returns only files
+            contents=[content for content in contents if "contents" not in content.keys()]
+        else:
+            # returns custom error message on wrong filter
+            return f"error: '{kwargs['filter']}' is not a valid filter criteria. Available filters are 'dir' and 'file'"
+    if kwargs["t"]:
+        # sort the output rows by modifed time, latest first
+        contents=sorted(contents, key=lambda sort_key: sort_key["time_modified"], reverse=True)
+    if kwargs["r"]:
+        # reverses the output rows
+        contents=contents[::-1]
+    if kwargs["l"]:
+        info_table=""
+        for content in contents:
+            # coverts the epoch time into human readable date-time form
+            datetime_obj = datetime.fromtimestamp(content["time_modified"])
+            str_time=datetime.strftime(datetime_obj,"%b %d %H:%M")
+            row=f'{content["permissions"]} {content["size"]: >4} {str_time} {content["name"]}'                    
+            info_table+=f"{row}\n"
+        return info_table.strip()
     # returns only the names if no argument given
     name_list=[content["name"] for content in contents]
     content_names=" ".join(name_list)
     return content_names
+
+arg_dict={
+            'A': False,
+            'l': True,
+            'r': True,
+            't': True,
+            'filter': None,
+            'path': "parserk"
+        }
+
+print(read_info(**arg_dict))
--- tests/test_pyls.py
+++ tests/test_pyls.py
@@ -2,57 +2,74 @@ from ..pyls import read_info
 from .expected_outputs import *
 
 # the read_info takes only keyword arguments as dict
-arg_dict={
-            'A': False,
-            'l': False,
-            'r': False,
-            't': False,
-            'filter': None,
-            'path': False
-        }
+def get_arg_dict(inp_dict:dict)->dict:
+    arg_dict={
+                'A': False,
+                'l': False,
+                'r': False,
+                't': False,
+                'filter': None,
+                'path': False
+            }
+    arg_dict.update(inp_dict)
+    return arg_dict
 
 # when no arg supplied from cli
 # it should lists out the top level
 def test_no_args():
-    assert read_info(**arg_dict)== top_level_list
+    input_dict=get_arg_dict({})
+    assert read_info(**input_dict)== top_level_list
 
 # lists out the top level with hidden files
 def test_args_a():
-    arg_dict['A']=True
-    assert read_info(**arg_dict)==top_level_hidden
+    input_dict=get_arg_dict({'A':True})
+    assert read_info(**input_dict)==top_level_hidden
 
 # prints detailed info
 def test_args_l():
-    arg_dict['l']=True
-    assert read_info(**arg_dict)==output_of_l
+    input_dict=get_arg_dict({'l':True})
+    assert read_info(**input_dict)==output_of_l
 
-# prints detailed info in reverse
+# # prints detailed info in reverse
 def test_args_l_r():
-    arg_dict['l']=True
-    arg_dict['r']=True
-    assert read_info(**arg_dict)==output_of_l_r
+    input_dict=get_arg_dict({'l':True, 'r':True})
+    assert read_info(**input_dict)==output_of_l_r
 
-# prints detailed info in reverse sorted by time modified
-def test_args_l_r_t():
-    arg_dict['l']=True
-    arg_dict['r']=True
-    arg_dict['t']=True
-    assert read_info(**arg_dict)==output_of_l_r_t
+# # prints detailed info sorted by time modified (latest first)
+def test_args_l_t():
+    input_dict=get_arg_dict({'l':True,'t':True})
+    assert read_info(**input_dict)==output_of_l_t
 
-# prints only dirs
+# # prints only dirs, sorted by time modified
 def test_args_filter_dir():
-    arg_dict['l']=True
-    arg_dict['filter']='dir'
-    assert read_info(**arg_dict)==output_of_filter_dir
+    input_dict=get_arg_dict({'l':True,'t':True,'filter':'dir'})
+    assert read_info(**input_dict)==output_of_filter_dir
 
-# prints only files
+# # prints only files, sorted by time modified
 def test_args_filter_file():
-    arg_dict['l']=True
-    arg_dict['filter']='file'
-    assert read_info(**arg_dict)==output_of_filter_file
+    input_dict=get_arg_dict({'l':True,'t':True,'filter':'file'})
+    assert read_info(**input_dict)==output_of_filter_file
 
 # prints details info of the path given
+# sorted by time modified and reversed output
 def test_args_path():
-    arg_dict['l']=True
-    arg_dict['path']='parser'
-    assert read_info(**arg_dict)==output_of_path
+    input_dict=get_arg_dict({'l':True,'t':True,'r':True, 'path':'parser'})
+    assert read_info(**input_dict)==output_of_path
+
+# if the path is a file, it prints the file itself
+def test_args_file_path():
+    input_dict=get_arg_dict({'l':True, 'path':'parser/parser.go'})
+    assert read_info(**input_dict)==excepted_file_path
+
+# print error massage if given filter other than 'dir/file'
+def test_filter_error():
+    input_dict=get_arg_dict({'filter':'folder'})
+    assert read_info(**input_dict)==excepted_filter_error
+
+def test_path_error():
+    input_dict=get_arg_dict({'path':'non_existent_path'})
+    assert read_info(**input_dict)==excepted_path_error
+
+
+
+
